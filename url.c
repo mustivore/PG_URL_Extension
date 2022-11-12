c7230b367054c4816f3c46e45fb1377a9c16c51a
mustivore <mustapha-ayadi@hotmail.be>
2022-11-12 20:06:19 +0100

    url_in & url_out

--- url.c
+++ url.c
@@ -1,23 +1,28 @@
-/*
- * example PostgreSQL input/output function for bigint
- *
- * To learn how to write code for PostgreSQL extensions:
- * C-Language Functions (https://www.postgresql.org/docs/13/xfunc-c.html)
- */
-/* Add required libraries here */
-
 #include <stdio.h>
 #include "postgres.h"
 #include <uriparser/Uri.h>
+#include <utils/builtins.h>
 
 PG_MODULE_MAGIC;
 
-static void parse_uri(const char *s, UriUriA *urip)
+typedef struct _url {
+   char* scheme;
+   char* host;
+   char* path;
+   char* query;
+   char* port;
+   char* url;
+   //char* protocol;
+}  URL;
+
+static inline void parse_url(const char* url_str, URL *url)
 {
+	UriUriA uri;
 	UriParserStateA state;
 
-	state.uri = urip;
-	uriParseUriA(&state, s);
+	state.uri = &uri;
+
+	uriParseUriA(&state, url_str);
 
 	if(state.errorCode != URI_SUCCESS){
 		ereport(ERROR,
@@ -25,29 +30,34 @@ static void parse_uri(const char *s, UriUriA *urip)
 							errmsg("invalid input syntax for type uri at or near \"%s\"",
 								state.errorPos)));
 	}
+	strcpy(url->scheme, uri.scheme.first);
+	strcpy(url->path, uri.userInfo.first);
+	strcpy(url->query, uri.query.first); 
+	strcpy(url->port, uri.portText.first);
+	strcpy(url->url, url_str);
+	uriFreeUriMembersA(&uri);
 }
 
 Datum url_in(PG_FUNCTION_ARGS);
 Datum url_out(PG_FUNCTION_ARGS);
 
+typedef struct varlena uritype;
+
 PG_FUNCTION_INFO_V1(url_in);
 Datum 
 url_in(PG_FUNCTION_ARGS) 
 {
-	char *s = PG_GETARG_CSTRING(0);
-	UriUriA uri;
+	char* url_str = PG_GETARG_CSTRING(0);
 
-	parse_uri(s, &uri);
-	uriFreeUriMembersA(&uri);
-
-	//vardata = (uritype *) cstring_to_text(s);
-	PG_RETURN_POINTER(uri);
+	URL* url = (URL*) palloc(sizeof(URL));
+	parse_url(url_str, url);
+	PG_RETURN_POINTER(url);
 }
 
 PG_FUNCTION_INFO_V1(url_out);
 Datum 
 url_out(PG_FUNCTION_ARGS) 
 {
-	Datum datum = PG_GETARG_DATUM(0);
-	PG_RETURN_CSTRING(TextDatumGetCString(datum));
+	URL* url = (URL *) PG_GETARG_POINTER(0);
+	PG_RETURN_CSTRING(url->url);
 }
