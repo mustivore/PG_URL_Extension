5f577b87ea4f855178b18c8d5097b5573576b8cb
gwafflar <guillaume.wafflard@ulb.be>
2022-12-05 18:34:15 +0100

    Reverting to previous commit

--- test_file.sql
+++ test_file.sql
@@ -11,6 +11,7 @@ CREATE TABLE test (
 INSERT INTO test(id, link) VALUES (1, 'http://www.google.com');
 INSERT INTO test(id, link) VALUES (2, 'https://www.google.com');
 INSERT INTO test(id, link) VALUES (3, 'https://www.google.com:443');
+--INSERT INTO test(id, link) VALUES (3, 'www.google.com'); --should not work
 INSERT INTO test(id, link) VALUES (4, 'https://www.ulb.be');
 INSERT INTO test(id, link) VALUES (5, 'https://www.ulb.ac.be');
 INSERT INTO test(id, link) VALUES (6, 'https://cs.ulb.ac.be/public/teaching/infoh415#exercices');
@@ -23,19 +24,19 @@ SELECT * from test;
 
 
 SELECT 	id, 
-		getAuthority(link), 
-		getDefaultPort(link), 
-		getFile(link), 
-		getHost(link), 
-		getPath(link), 
-		getPort(link), 
-		getProtocol(link), 
-		getQuery(link), 
-		getRef(link), 
-		getUserInfo(link) 
+		get_authority(link), 
+		get_default_port(link), 
+		get_file(link), 
+		get_host(link), 
+		get_path(link), 
+		get_port(link), 
+		get_protocol(link), 
+		get_query(link), 
+		get_ref(link), 
+		get_user_info(link) 
 FROM test 
 WHERE 
 		--equals(link, 'https://www.google.com:443')
 		--same_file(link, 'https://cs.ulb.ac.be/public/teaching/infoh415#project')
-		sameHost(link, 'https://www.ulb.be')
+		same_host(link, 'https://www.ulb.be')
 		;
