2d2c0e0ba6c47aa183dcfbfd8208a09e984c9c0a
mustivore <mustapha-ayadi@hotmail.be>
2022-12-09 17:13:23 +0100

    fix test

--- test_file_index.sql
+++ test_file_index.sql
@@ -19,12 +19,11 @@ INSERT INTO test(url_db) VALUES ('https://uv.ulb.ac.be/enrol/index.php?id=106500
 INSERT INTO test(url_db) VALUES ('https://uv.ulb.ac.be/pluginfile.php/3623050/mod_resource/content/1/Project2%20-%20DB%20Extension.pdf');
 
 -- We can see thanks to the query plan that the index is triggered while computing equals,sameHost and sameFile
-
-EXPLAIN SELECT t1.url_db FROM test t1, test t2 where t1.id <> t2.id and equals(t1.url_db, t2.url_db); -- 0 rows
+EXPLAIN SELECT t1.url_db FROM test t1, test t2 where t1.id <> t2.id and equals(t1.url_db, t2.url_db); 
 EXPLAIN SELECT t1.url_db FROM test t1, test t2 where t1.id <> t2.id and sameHost(t1.url_db, t2.url_db);
 EXPLAIN SELECT t1.url_db FROM test t1, test t2 where t1.id <> t2.id and sameFile(t1.url_db, t2.url_db);
 
-SELECT t1.url_db FROM test t1, test t2 where t1.id <> t2.id and equals(t1.url_db, t2.url_db);
+SELECT t1.url_db FROM test t1, test t2 where t1.id <> t2.id and equals(t1.url_db, t2.url_db); -- 0 rows
 SELECT t1.url_db FROM test t1, test t2 where t1.id <> t2.id and sameHost(t1.url_db, t2.url_db);
 SELECT t1.url_db FROM test t1, test t2 where t1.id <> t2.id and sameFile(t1.url_db, t2.url_db);
 
